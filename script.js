208aa59d0ad8c839fd01d2b98640d72091f9b078
Praveennaik6 <praveennaik0814@gmail.com>
2025-04-08 21:02:52 +0530

    changes in css, javascript files

---
+++ script.js
@@ -0,0 +1,9 @@
+const BASE_URL ="https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+
+const dropdowns=document.querySelector(".dropdown select");
+
+for(let select of dropdowns){
+    for(code in countryList){
+
+    }
+}
